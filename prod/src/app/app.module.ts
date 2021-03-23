c62acb6eb83172e35b2adff28dc802950607779f
Neon_Samurai <75180540+samurai-2000@users.noreply.github.com>
2021-03-23 13:40:39 +0400

    update start

---
+++ prod/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiHeaderComponent } from './components/ui-header/ui-header.component';
+import { MainComponent } from './components/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UiHeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
