a148ca0a1ce0c53ea34f1a90f983ddcdb311c5a9
Neon_Samurai <75180540+samurai-2000@users.noreply.github.com>
2021-04-29 18:33:54 +0400

    update 1.62

--- prod/src/app/components/brief/brief.component.ts
+++ prod/src/app/components/brief/brief.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
 import { BriefService } from 'src/app/services/brief.service';
 import { TrackingService } from 'src/app/services/tracking.service';
 
@@ -37,6 +38,7 @@ export class BriefComponent implements OnInit {
     private track: TrackingService,
     private router: Router,
     private title: Title,
+    private app: AppComponent,
   ) {
     this.title.setTitle('Бриф')
   }
@@ -50,7 +52,12 @@ export class BriefComponent implements OnInit {
     .subscribe((res: any) => {
       this.track.setLocalTrack(res)
       this.router.navigate(['/tracking'])
-    })
+    },
+    () => false,
+    () => setTimeout(() => 
+    { this.app.openModal('Уведомление',
+    `Спасибо за то, что заполнили бриф, в данный момент вам на почтуотправлено письмо 
+    с номером вашего заказа, так же вы можете в любой момент отследить процесс разработки вашего проекта`)}, 1000))
   }
 
 }
